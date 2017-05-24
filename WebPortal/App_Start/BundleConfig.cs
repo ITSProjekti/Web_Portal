7aa9ffcf7c9a4348d1ce814347102c1f100ba2cb
Mladen <mladen@test.com>
2017-05-24 14:01:09 +0200

    Dodavanje bootstrap teme.

--- WebPortal/App_Start/BundleConfig.cs
+++ WebPortal/App_Start/BundleConfig.cs
@@ -24,8 +24,12 @@ namespace WebPortal
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      //"~/Content/bootstrap.css",
+                      "~/Content/css/reset.css",
+                      "~/Content/css/bootstrap-flatly.css",
+                      "~/Content/css/site.css",
+                      "~/Content/css/izgled.css"
+                      ));
         }
     }
 }
